e32258097e56f48629c7142454a12f03a7acf57e
PavliukKonstantin <Pavliuk.Konstantin@gmail.com>
2020-09-29 10:04:38 +0300

    Replace KDF on Script

--- one_time_secret/app/crypto.py
+++ one_time_secret/app/crypto.py
@@ -4,8 +4,7 @@ import os
 
 from cryptography.fernet import Fernet, InvalidToken
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
 
 def _get_key(code_phrase: bytes, salt: bytes) -> bytes:
@@ -19,21 +18,23 @@ def _get_key(code_phrase: bytes, salt: bytes) -> bytes:
     Returns:
         bytes: encryption key.
     """
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
+    kdf = Scrypt(
         salt=salt,
-        iterations=100000,
-        backend=default_backend())
+        length=32,
+        n=2**14,
+        r=8,
+        p=1,
+        backend=default_backend()
+    )
     return base64.urlsafe_b64encode(kdf.derive(code_phrase))
 
 
 def _get_salt():
     """
-    Get random generated 32 bytes.
+    Get random generated 16 bytes.
 
     Returns:
-        bytes: random generated 32 bytes.
+        bytes: random generated 16 bytes.
     """
     return os.urandom(16)
 
